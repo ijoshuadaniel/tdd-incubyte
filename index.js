eab84cde7673b52a5b4209741a972acad7864f34
Josh <josh@Joshs-Laptop.local>
2024-02-09 17:04:11 +0530

    removed comments and seperated into ddifferent functions

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-const add = (numbers) => {
-  // If numbers is empty or undefined, return 0
-  if (numbers === "") {
-    return 0;
-  }
-
-  // Adding delimiters
-  let delimiters = [",", "\n"];
-  // Negative numbers array
-  let negativeNumbers = [];
-
-  // Supporting custom delimiter
-  if (numbers.startsWith("//")) {
-    const customDelimiter = numbers.substring(2, numbers.indexOf("\n"));
-    delimiters = [customDelimiter];
-    numbers = numbers.substring(numbers.indexOf("\n") + 1);
-  }
-
-  // Creating regex for delimiters
-  const regex = new RegExp(`[${delimiters.join("")}]`);
-  // Adding numbers
-  const allNumbers = numbers.split(regex).map((int) => {
+const splitNumbers = (numbers, regex, negativeNumbers) => {
+  return numbers.split(regex).map((int) => {
     const parsedNumber = Number(int);
     if (parsedNumber < 0) {
       negativeNumbers.push(parsedNumber);
     }
     return parsedNumber;
   });
-
-  // If negative numbers are present, throw an error
-  if (negativeNumbers.length > 0) {
-    throw new Error(
-      "Negative numbers are not allowed: " + negativeNumbers.join(", ")
-    );
-  }
-
-  return allNumbers.reduce((a, b) => a + b);
 };
 
-console.log(add(""));
-console.log(add("1,2"));
-console.log(add("4\n2"));
-console.log(add("//;\n1;2;3;4"));
+const throwNegativeError = (negativeNumbers) => {
+  throw new Error(
+    "Negative numbers are not allowed: " + negativeNumbers.join(", ")
+  );
+};
 
-try {
-  console.log(add("1,2,3,4,5,-6"));
-} catch (error) {
-  console.error(error.message);
-}
+const sumNumbers = (numbers) => {
+  return numbers.reduce((a, b) => a + b);
+};
+
+const add = (numbers) => {
+  if (numbers === "") return 0;
+
+  let delimiters = [",", "\n"];
+  let negativeNumbers = [];
+
+  if (numbers.startsWith("//")) {
+    const customDelimiter = numbers.substring(2, numbers.indexOf("\n"));
+    delimiters = [customDelimiter];
+    numbers = numbers.substring(numbers.indexOf("\n") + 1);
+  }
+
+  const regex = new RegExp(`[${delimiters.join("")}]`);
+  const allNumbers = splitNumbers(numbers, regex, negativeNumbers);
+
+  if (negativeNumbers.length > 0) {
+    throwNegativeError(negativeNumbers);
+  }
+
+  return sumNumbers(allNumbers);
+};
 
 module.exports = add;
